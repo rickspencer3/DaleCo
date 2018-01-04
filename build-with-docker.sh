b48c10ed498731fd825c5f327b37536113a6aae9
Wojciech Kocjan <wojciech@kocjan.org>
2018-01-04 10:29:36 +0100

    Fix build-with-docker.sh maven target

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -4,4 +4,4 @@ set -eux
 
 cd "$(dirname "$(basename "$0")")"
 
-docker run --rm -v $(pwd):/app -w /app maven:3.5.2-jdk-8 mvn war:war
+docker run --rm -v $(pwd):/app -w /app maven:3.5.2-jdk-8 mvn package
