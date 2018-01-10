c68a87a3d9fc119711d5bda9454c89a271b44724
Bridget McErlean <bridget@bitnami.com>
2018-01-10 17:13:51 +0000

    Select driver depending on database type

--- src/daleco/database/DatabaseConfig.java
+++ src/daleco/database/DatabaseConfig.java
@@ -4,21 +4,27 @@ public class DatabaseConfig {
 	private final String user;
 	private final String password;
 	private final String host;
-	private final String databaseName;
+	private final String name;
+	private final String type;
 
-	public DatabaseConfig(String user, String password, String host, String databaseName) {
+	public DatabaseConfig(String user, String password, String host, String name, String type) {
 		this.user = user;
 		this.password = password;
 		this.host = host;
-		this.databaseName = databaseName;
+		this.name = name;
+		this.type = type;
 	}
 
 	public String GetHost() {
 		return this.host;
 	}
 
-	public String GetDatabaseName() {
-		return this.databaseName;
+	public String GetName() {
+		return this.name;
+	}
+
+	public String GetType() {
+		return this.type;
 	}
 
 	public String GetUser() {
