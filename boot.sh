cad19ef7aa2c56bd4f3acf8971297ad55d59727f
Matt Goodall <matt.goodall@gmail.com>
2018-03-13 11:59:29 +0000

    Keep & extend the env var documentation

--- boot.sh
+++ boot.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-#use set to enable verbose output
+# This script configures the DaleCo example web app according to the following
+# environment variables:
+#
+#   DATABASE_TYPE:
+#       The type of database server - "mysql" or "postgres"
+#   DATABASE_HOST:
+#       The hostname to use to connect to the database
+#   DATABASE_NAME:
+#       The name of the database.
+#   DATABASE_USER:
+#       The root username for the database. You can connect as this user and
+#       create other users if you require
+#   DATABASE_PASSWORD:
+#       The password for the root database user
+
+
+# use set to enable verbose output
 set -euo pipefail
 
 readonly CONF=WEB-INF/classes/database.properties
 
-# Change to webapp directory. Assumes there's only one webapp unpacked to tomcat.
+# change to webapp directory. Assumes there's only one webapp unpacked to tomcat.
 cd $(find /var/lib/tomcat/webapps/ -type d -mindepth 1 -maxdepth 1 | head -n 1)
 
 echo "current directory: " $PWD
 echo "writing to file: " $CONF
 
-# Write database config
+# write database config
 cat > "${CONF}" <<-EOF
 type=${DATABASE_TYPE}
 host=${DATABASE_HOST}
